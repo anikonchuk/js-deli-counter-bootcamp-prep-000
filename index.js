156aa38731c29dd50ef08aa7a9c7d3bc281f5989
Andrew Nikonchuk <anikonch@gmail.com>
2018-04-02 14:38:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (var i = 0, i < katzDeliLine.length, i++){
-      lineSpot.push(` ${i+1}. ${katzDeliLine[i]}`);
+    for (var i = 0, i < katzDeliLine.length, i++) {
+      lineSpot.push(` ${[i+1]}. ${katzDeliLine[i]}`);
     }
     return `The line is currently:${lineSpot}`;
   } 
